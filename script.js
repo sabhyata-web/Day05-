2dd63207427d42d97087d5ce7702a48c7149d03c
sabhyata-web <sabhyatagyawali285@gmail.com>
2025-01-08 08:40:12 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let ham = document.querySelector("hamburger");
+
+let navList = document.querySelector("nav-list");
+
+ham.addEventListener("click", function () {
+  //   console.log("clicked");
+  navLList.classList.toggle("clicked");
+});
